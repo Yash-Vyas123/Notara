9d331a937f645a0341cd9a42dd70d7569d9b6480
Yash-Vyas123 <yashvyasnbh@gamil.com>
2026-03-12 20:40:03 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import path from "path";
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -14,13 +14,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001
 
-
+const __dirname = path.resolve();
 
 //middleware :- it is a function that runs in the middle between the request and the response.
+if(process.env.NODE_ENV !== "production") {
 app.use(
     cors({
         origin: "http://localhost:5173",
     }));  
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -34,7 +36,15 @@ next();
 
 
 app.use("/api/notes", notesRoutes);
-  
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+if(process.env.NODE_ENV == "production"){
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+}
+
 connectDB().then(()=>{
 app.listen(PORT, () =>{
     console.log("Server Started on PORT:", PORT);
